40f52480badf88d6cf1a31e86478c9f8a10c35dd
Alexeyco <6425762@gmail.com>
2011-05-06 20:57:37 +0400

    Media file type checking added

--- classes/controller/media.php
+++ classes/controller/media.php
@@ -2,20 +2,27 @@
 
 class Controller_Media extends Controller {
 
+	protected $_config = 'media';
+
+	public function before()
+	{
+		parent::before();
+
+		// Attach configuration
+		$config = Kohana::config($this->_config);
+	}
+
 	public function action_index($environment)
 	{
-		// Attach configuration
-		$config = Kohana::config('media');
-
 		// Get the file path from the request
 		$file = $this->request->param('file');
 
-		if ( ! isset($config[$environment]))
+		if ( ! isset($this->_config[$environment]))
 		{
 			return $this->response->status(404);
 		}
 
-		$config = $config[$environment];
+		$config = $this->_config[$environment];
 
 		$files = explode(Media::$delimiter, $file);
 
@@ -24,10 +31,19 @@ class Controller_Media extends Controller {
 			$files = array($files);
 		}
 
+		$extension = pathinfo($file, PATHINFO_EXTENSION);
+
+		// Improve file content protection
+		if ((is_array($config['types']) AND ! in_array($extension, $config['types'])) OR
+			( ! is_array($config['types']) AND $extension != $config['types']))
+		{
+			return $this->response->status(404);
+		}
+
 		$media = Media::instance($environment);
 
 		// Set the proper headers to allow caching
-		$this->response->headers('content-type', File::mime_by_ext(pathinfo($file, PATHINFO_EXTENSION)));
+		$this->response->headers('content-type', File::mime_by_ext($extension));
 
 		// Send the file content as the response
 		$this->response->body($media->minify_files($file));
